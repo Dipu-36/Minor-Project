a5f531e7b4835c3de3903028041d529578cf3910
root <root@LAPTOP-DUC7G7NC.>
2025-11-24 17:09:20 +0000

    Added the reference files

--- wasm_crypto/ed25519_ref/ed25519.h
+++ wasm_crypto/ed25519_ref/ed25519.h
@@ -2,17 +2,34 @@
 #define ED25519_H
 
 #include <stddef.h>
-#include <stdint.h>
+
+#if defined(_WIN32)
+    #if defined(ED25519_BUILD_DLL)
+        #define ED25519_DECLSPEC __declspec(dllexport)
+    #elif defined(ED25519_DLL)
+        #define ED25519_DECLSPEC __declspec(dllimport)
+    #else
+        #define ED25519_DECLSPEC
+    #endif
+#else
+    #define ED25519_DECLSPEC
+#endif
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Function declarations - we'll provide simple implementations
-void ed25519_sha512_hash(uint8_t *hash, const uint8_t *in, size_t inlen);
-void ed25519_randombytes(uint8_t *out, size_t outlen);
-void ed25519_scalarmult_base(uint8_t *out, const uint8_t *scalar);
-void ed25519_scalar_muladd(uint8_t *out, const uint8_t *a, const uint8_t *b, const uint8_t *c);
+#ifndef ED25519_NO_SEED
+int ED25519_DECLSPEC ed25519_create_seed(unsigned char *seed);
+#endif
+
+void ED25519_DECLSPEC ed25519_create_keypair(unsigned char *public_key, unsigned char *private_key, const unsigned char *seed);
+void ED25519_DECLSPEC ed25519_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key, const unsigned char *private_key);
+int ED25519_DECLSPEC ed25519_verify(const unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key);
+void ED25519_DECLSPEC ed25519_add_scalar(unsigned char *public_key, unsigned char *private_key, const unsigned char *scalar);
+void ED25519_DECLSPEC ed25519_key_exchange(unsigned char *shared_secret, const unsigned char *public_key, const unsigned char *private_key);
+
 
 #ifdef __cplusplus
 }
